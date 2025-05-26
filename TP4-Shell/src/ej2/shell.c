b64b7ff05a752a431dd0bccdac76cc872e4ec9fd
ValenPettaz1 <vpettazi@udesa.edu.ar>
2025-05-26 15:10:25 -0300

    intro ej2

--- TP4-Shell/src/ej2/shell.c
+++ TP4-Shell/src/ej2/shell.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #define MAX_COMMANDS 200
+#define MAX_ARGS 64
 
 int main() {
 
@@ -24,7 +25,15 @@ int main() {
            The strcspn() function returns the length of the initial segment of command that consists of 
            characters not in the string specified in the second argument ("\n" in this case). */
         command[strcspn(command, "\n")] = '\0';
+        
+        // Verificar si el usuario quiere salir
+        if (strcmp(command, "exit") == 0) {
+            break;
+        }
 
+        // Reiniciar el contador de comandos
+        command_count = 0;
+        
         /* Tokenizes the command string using the pipe character (|) as a delimiter using the strtok() function. 
            Each resulting token is stored in the commands[] array. 
            The strtok() function breaks the command string into tokens (substrings) separated by the pipe character |. 
@@ -37,11 +46,88 @@ int main() {
             token = strtok(NULL, "|");
         }
 
-        /* You should start programming from here... */
-        for (int i = 0; i < command_count; i++) 
-        {
-            printf("Command %d: %s\n", i, commands[i]);
-        }    
+        if (command_count == 0) {
+            continue;  // Si no hay comandos, continuar
+        }
+
+        // Crear los pipes necesarios (uno menos que el número de comandos)
+        int pipes[MAX_COMMANDS - 1][2];
+        for (int i = 0; i < command_count - 1; i++) {
+            if (pipe(pipes[i]) == -1) {
+                perror("Error al crear pipe");
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        // Ejecutar cada comando en un proceso hijo
+        pid_t pids[MAX_COMMANDS];
+        for (int i = 0; i < command_count; i++) {
+            // Parsear el comando en programa y argumentos
+            char *args[MAX_ARGS];
+            int arg_count = 0;
+            
+            // Eliminar espacios iniciales
+            char *cmd = commands[i];
+            while (*cmd == ' ') cmd++;
+            
+            // Tokenizar por espacios para separar argumentos
+            char *arg = strtok(cmd, " ");
+            while (arg != NULL && arg_count < MAX_ARGS - 1) {
+                args[arg_count++] = arg;
+                arg = strtok(NULL, " ");
+            }
+            args[arg_count] = NULL;  // Terminar array con NULL para execvp
+            
+            if (arg_count == 0) continue;
+            
+            // Crear proceso hijo
+            pids[i] = fork();
+            
+            if (pids[i] < 0) {
+                perror("Error en fork");
+                exit(EXIT_FAILURE);
+            }
+            
+            if (pids[i] == 0) {  // Código del proceso hijo
+                // Configurar redirección de entrada (si no es el primer comando)
+                if (i > 0) {
+                    dup2(pipes[i-1][0], STDIN_FILENO);
+                }
+                
+                // Configurar redirección de salida (si no es el último comando)
+                if (i < command_count - 1) {
+                    dup2(pipes[i][1], STDOUT_FILENO);
+                }
+                
+                // Cerrar todos los descriptores de archivo de pipes
+                for (int j = 0; j < command_count - 1; j++) {
+                    close(pipes[j][0]);
+                    close(pipes[j][1]);
+                }
+                
+                // Ejecutar el comando
+                execvp(args[0], args);
+                
+                // Si llegamos aquí, execvp falló
+                fprintf(stderr, "Error al ejecutar: %s\n", args[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        // Cerrar todos los pipes en el proceso padre
+        for (int i = 0; i < command_count - 1; i++) {
+            close(pipes[i][0]);
+            close(pipes[i][1]);
+        }
+        
+        // Esperar a que todos los procesos hijo terminen
+        for (int i = 0; i < command_count; i++) {
+            waitpid(pids[i], NULL, 0);
+        }
+        
+        // Reiniciar para el próximo comando
+        command_count = 0;
     }
+    
     return 0;
 }
